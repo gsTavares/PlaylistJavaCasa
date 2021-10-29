f6466edaabd613276ff5d03457d25f1989dd515c
Gustavo Tavares <gustavo.tavares24@outlook.com>
2021-10-29 17:59:25 -0300

    Introdução às classes utilitárias

---
+++ src/com/aulas/maratonajava/classes/utilitarias/WrapperTest.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.aulas.maratonajava.classes.utilitarias;
+
+/**
+ *
+ * @author Marketing
+ */
+public class WrapperTest {
+    public static void main(String[] args) {
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10L;
+        float floatP = 10F;
+        double doubleP = 100;
+        char charP = 'a';
+        boolean booleanP = false;
+        
+        // Wrappers -> classes que encapsulam os tipos primitivos
+        // Úteis para passagem de parâmetro por referência
+        
+        Byte byteW = 1;
+        Short shortW = 1;
+        Integer intW = 1;
+        Long longW = 10L;
+        Float floatW = 10F;
+        Double doubleW = 100.0;
+        Character charW = 'a';
+        Boolean booleanW = false;
+    }
+}
